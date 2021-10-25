f507b143f1f1076d8cf3e1cf6e0e4ebea3ac8b13
realugi <u.jovanovic@teletronic.at>
2021-10-25 10:25:26 +0200

    Initial commit

---
+++ src/components/TodoList.vue
@@ -0,0 +1,20 @@
+<template>
+  <p>Hello World!</p>
+</template>
+
+<script>
+export default {
+  name: 'TodoList',
+  data () {
+    return {
+      todos: []
+    }
+  },
+  created () {
+    var todoApi = this.$resource('todos{/id}')
+    todoApi.get().then(res => {
+      this.todos = res.body
+    })
+  }
+}
+</script>
