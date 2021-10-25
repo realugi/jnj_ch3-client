27f83dc1b2080ea6a899a2c5161757fd90271396
realugi <u.jovanovic@teletronic.at>
2021-10-25 10:25:36 +0200

    Implement initial todo listing

---
+++ src/components/TodoElement.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p>{{ description }}</p>
+    <p>{{ done }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoElement',
+  props: ['description', 'done']
+}
+</script>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,10 +1,22 @@
 <template>
-  <p>Hello World!</p>
+  <div>
+    <todo-element
+      v-for="todo in todos"
+      v-bind:key="todo.id"
+      v-bind:description="todo.description"
+      v-bind:done="todo.done"
+    ></todo-element>
+  </div>
 </template>
 
 <script>
+import TodoElement from '@/components/TodoElement'
+
 export default {
   name: 'TodoList',
+  components: {
+    'todo-element': TodoElement
+  },
   data () {
     return {
       todos: []
