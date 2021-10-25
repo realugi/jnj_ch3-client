6e4b626d6884ae5a3f5fc8b0dec1dce350e9d67b
realugi <u.jovanovic@teletronic.at>
2021-10-25 10:25:44 +0200

    Improve client rest-api

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -23,27 +23,28 @@ export default {
   },
   data () {
     return {
-      todos: []
+      todos: [],
+      api: this.$resource('todos{/id}')
     }
   },
   methods: {
     addTodo (todo) {
-      this.$resource('todos{/id}').save(todo).then(res => this.todos.push(res.body))
+      this.api.save(todo).then(res => this.todos.push(res.body))
     },
     updateTodo (todo) {
-      this.$resource('todos{/id}').update({id: todo.id}, todo).then(res => {
+      this.api.update({id: todo.id}, todo).then(res => {
         this.todos = this.todos.map(e => (e.id === res.body.id) ? res.body : e)
       })
     },
     deleteTodo (todo) {
-      this.$resource('todos{/id}').delete({id: todo.id}).then(res => {
+      this.api.delete({id: todo.id}).then(res => {
         const index = this.todos.findIndex(e => e.id === todo.id)
         this.todos = this.todos.slice(0, index).concat(this.todos.slice(index + 1))
       })
     }
   },
   created () {
-    this.$resource('todos{/id}').get().then(res => {
+    this.api.get().then(res => {
       this.todos = res.body
     })
   }
